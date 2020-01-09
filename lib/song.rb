9eb414dfb357b191fdffb0a7f2f67e0452a102c4
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-09 00:47:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,13 +33,12 @@ class Song
   end 
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) ||self.create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end 
 
 
   def self.alphabetical
     @@all.sort 
-  end 
   end 
 
 end
